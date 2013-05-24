83331be188eb0a1f9dab11669fee9d8bb3c05194
Mikeva <eva091960601@gmail.com>
2013-05-24 15:42:21 +0800

    test cross

--- public/js/event.js
+++ public/js/event.js
@@ -44,10 +44,10 @@ $(document).on('mousemove',function(e){
 
 $(".about").on("click",function(){
 	$("#main_page").animate({
-				$("#main_page").fadeOut();
+				$("#main_page").fadeOut(1000);
 			},1000,function(){
 				$("#main_page").load("about.html .about_page")
-				$("#main_page").fadeIn();
+				$("#main_page").fadeIn(1000);
 			});
 	
 });
