24951ba53c178b52d8dff424e33de1b25dd1053f
Mikeva <eva091960601@gmail.com>
2013-07-23 23:34:52 +0800

    new page create

--- public/js/event.js
+++ public/js/event.js
@@ -1,3 +1,58 @@
+function firstctrl($scope){
+	$scope.data = {message: "panel"}
+}
+
+$("#leftimg").on('mouseenter',function(){
+	$("#leftimg").prepend("<div id='white_mask' class='mask'><div class='text_content'>About me</div></div>")
+});
+$("#leftimg").on('mouseleave',function(){
+	$("#white_mask").remove();
+});
+$("#rightimg").on('mouseenter',function(){
+	$("#rightimg").prepend("<div id='black_mask' class='mask'><div class='text_content'>Project</div></div>")
+});
+$("#rightimg").on('mouseleave',function(){
+	$("#black_mask").remove();
+});
+$(document).ready(function(){
+	$("#left").hide();
+	$("#leftimg").click(function(){
+		// window.setTimeout(function(){},)
+		
+		$("#middle").animate({
+			opacity: 0
+		},500,function(){
+			window.setTimeout(function(){
+				$("#left").show();
+				$("#middle").hide();
+			},200);
+			
+			
+		});
+		
+	});
+
+	$("#right").hide();
+	$("#rightimg").click(function(){
+		// window.setTimeout(function(){},)
+		
+		$("#middle").animate({
+			opacity: 0
+		},500,function(){
+			window.setTimeout(function(){
+				$("#right").show();
+				$("#middle").hide();
+			},200);
+			
+			
+		});
+		
+	});
+});
+
+
+
+
 a = 0 ;
 
 $(document).on('mousemove',function(e){
