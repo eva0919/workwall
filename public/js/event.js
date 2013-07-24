365bd9ba1dfcf6aca05d799144e23c70c2d8b4df
Mikeva <eva091960601@gmail.com>
2013-07-24 20:53:29 +0800

    animate loading2

--- public/js/event.js
+++ public/js/event.js
@@ -1,4 +1,27 @@
-
+// spin
+var opts = {
+  lines: 13, // The number of lines to draw
+  length: 20, // The length of each line
+  width: 10, // The line thickness
+  radius: 30, // The radius of the inner circle
+  corners: 1, // Corner roundness (0..1)
+  rotate: 0, // The rotation offset
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  color: '#fff', // #rgb or #rrggbb
+  speed: 1.2, // Rounds per second
+  trail: 60, // Afterglow percentage
+  shadow: true, // Whether to render a shadow
+  hwaccel: false, // Whether to use hardware acceleration
+  className: 'spinner', // The CSS class to assign to the spinner
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  top: 'auto', // Top position relative to parent in px
+  left: 'auto' // Left position relative to parent in px
+};
+var target = document.getElementById('hoo');
+var spinner = new Spinner(opts).spin(target);
+// var spinner = new Spinner().spin();
+// target.appendChild(spinner.el);
+// spin end
 function firstctrl($scope){
 	$scope.data = {message: "panel"}
 }
